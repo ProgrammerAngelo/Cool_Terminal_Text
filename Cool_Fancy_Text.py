5c0dab98e659b569d2f78208bb9fe39ac96fd14d
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-03-23 11:23:06 -0400

    Added tittle and Pseudocode

---
+++ Cool_Fancy_Text.py
@@ -0,0 +1,26 @@
+print("Assignment #3")
+
+#Pseudocode
+
+#Add pyfiglet to use for fonts
+
+#Add input for name
+#Line for Printing name
+#Add a font & color for name
+
+#Add input for age
+#Line for Printing age
+#Add a font & color for age
+
+#Add input for sex
+#Line for Printing sex
+#Add a font & color for sex
+
+#Add input for dream job
+#Line for Printing dream job
+#Add a font & color for dream job
+
+#Add input for hobby
+#Line for Printing hobby
+#Add a font & color for hobby
+
