43f18c3a14530d28641fedf029b12347e9b21c2d
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-03-23 11:32:37 -0400

    Added colors to the text

--- Cool_Fancy_Text.py
+++ Cool_Fancy_Text.py
@@ -12,27 +12,27 @@ font_age= Figlet(font="digital")
 font_sex= Figlet(font="digital")
 
 #Add input for name
-name = input("Please enter your Name: ")
+name = input("\033[1;31m" + font_name.renderText("Please enter your Name: "))
 #Line for Printing name
 #Add a font & color for name
 
 #Add input for age
-age = input("Please enter your age: ")
+age = input("\033[1;30m" + font_age.renderText("Please enter your age: "))
 #Line for Printing age
 #Add a font & color for age
 
 #Add input for sex
-sex = input("Please enter your sex: ")
+sex = input("\033[1;32m" + font_job.renderText("Please enter your sex: "))
 #Line for Printing sex
 #Add a font & color for sex
 
 #Add input for dream job
-Dream_Job = input("Please enter your Dream Job: ")
+Dream_Job = input("\033[1;34m" + font_job.renderText("Please enter your dream Job: "))
 #Line for Printing dream job
 #Add a font & color for dream job
 
 #Add input for hobby
-hobby = input("Please enter your Hobby: ")
+hobby = input("\033[1;35m" + font_hobby.renderText("Please enter your Hobby: "))
 #Line for Printing hobby
 #Add a font & color for hobby
 
