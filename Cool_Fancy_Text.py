2b3355a1aa7c2a7cea9bfdf2ee0a14c2392e7dd7
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-03-23 11:26:43 -0400

    Added input for name,age,sex,dreamjob,hobby

--- Cool_Fancy_Text.py
+++ Cool_Fancy_Text.py
@@ -5,22 +5,27 @@ print("Assignment #3")
 #Add pyfiglet to use for fonts
 
 #Add input for name
+name = input("Please enter your Name: ")
 #Line for Printing name
 #Add a font & color for name
 
 #Add input for age
+age = input("Please enter your age: ")
 #Line for Printing age
 #Add a font & color for age
 
 #Add input for sex
+sex = input("Please enter your sex: ")
 #Line for Printing sex
 #Add a font & color for sex
 
 #Add input for dream job
+Dream_Job = input("Please enter your Dream Job: ")
 #Line for Printing dream job
 #Add a font & color for dream job
 
 #Add input for hobby
+hobby = input("Please enter your Hobby: ")
 #Line for Printing hobby
 #Add a font & color for hobby
 
